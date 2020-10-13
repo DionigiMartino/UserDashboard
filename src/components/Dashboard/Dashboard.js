b9b9b4f6bd6c98367f695bd01c8a48e62b285332
Dionigi Martino de Carles <40464068+DionigiMartino@users.noreply.github.com>
2020-10-13 10:50:18 +0200

    Setted the Photo & Anagraphics

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,16 +5,31 @@ class Dashboard extends Component {
     constructor(props){
         super(props);
         this.state = {
-            userName: '',
-            userJob: '',
+            userName: 'Dionigi Martino de Carles',
+            userJob: 'Programmatore',
             userPhoto: '',
-            userJobsList: []
+            userJobsList: [],
+            lastProject: 'Ovopiù',
+            nextProjects: 'Rosmino Srl'
         }
     }
+    
     render(){
         return(
             <div className={DashboardStyle.cont__dashboard}>
-                
+                <div className={DashboardStyle.cont__dashboard_anagraphics}>
+                    <img src={this.state.userPhoto} className={DashboardStyle.cont__dashboard_anagraphics_picImage} />
+                    
+                    <ul className={DashboardStyle.cont__dashboard_anagraphics_data}>
+                        <li className={DashboardStyle.cont__dashboard_anagraphics_data_el}>{this.state.userName} - {this.state.userJob}</li> 
+                        <li className={DashboardStyle.cont__dashboard_anagraphics_data_el}>Ultimo Lavoro: {this.state.lastProject}</li>
+                        <li className={DashboardStyle.cont__dashboard_anagraphics_data_el}>Prossimo Lavoro: {this.state.nextProjects}</li>
+                    </ul>
+                </div>
+
+                <div className={DashboardStyle.cont__dashboard_jobList}>
+
+                </div>
             </div>
         )
     }
